2176e6e794cb6255da6bde31e7d6ada2a0a8319f
crh <crh@hisihi.com>
2018-05-29 16:58:49 +0800

    初始化

---
+++ XYYKit/XYYKit.h
@@ -0,0 +1,13 @@
+//
+//  XYYKit.h
+//  XYYKit
+//
+//  Created by 陈荣航 on 2018/5/29.
+//  Copyright © 2018年 leslie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYYKit : NSObject
+
+@end
---
+++ XYYKit/XYYKit.m
@@ -0,0 +1,13 @@
+//
+//  XYYKit.m
+//  XYYKit
+//
+//  Created by 陈荣航 on 2018/5/29.
+//  Copyright © 2018年 leslie. All rights reserved.
+//
+
+#import "XYYKit.h"
+
+@implementation XYYKit
+
+@end
